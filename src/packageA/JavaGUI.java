c11d63c654f48c32ec852d8b14437f9240f1afc5
DESKTOP-ML87TGU\Winfred Villaluna <winfred_villaluna@dlsu.edu.ph>
2017-10-10 03:40:56 +0800

    Adds GUI and adds connector from g4 to GUI

--- src/packageA/JavaGUI.java
+++ src/packageA/JavaGUI.java
@@ -1,16 +1,38 @@
 package packageA;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Element;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class JavaGUI extends JFrame {
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+
+public class JavaGUI extends JFrame implements MouseListener{
 
 	private JPanel contentPane;
 
+	private JScrollPane scrollPane;
+	private JTextArea taField;
+	private JTextArea taLineNumber;
+	private JButton btnRun; 
+	
+	private JScrollPane spLexer;
+	private JTextArea taLexer;
+	private JScrollPane spResult;
+	private JTextArea taResult;
 	/**
 	 * Launch the application.
 	 */
@@ -31,12 +53,117 @@ public class JavaGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public JavaGUI() {
+		setTitle("COMPILER v0.1");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 978, 735);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setRowHeaderView(taLineNumber);
+		scrollPane.setBounds(10, 45, 678, 414);
+		contentPane.add(scrollPane);
+		scrollPane.setViewportView(taField);
+
+		taLineNumber = new JTextArea();  
+		taLineNumber.setFont(new Font("Consolas", Font.PLAIN, 13));
+		taLineNumber.setBackground(Color.LIGHT_GRAY);
+		taLineNumber.setText(" 1  " + System.getProperty("line.separator"));
+ 		taLineNumber.setEditable(false);
+ 				
+ 		taField = new JTextArea();       
+		taField.setFont(new Font("Consolas", Font.PLAIN, 13));
+		taField.getDocument().addDocumentListener(new DocumentListener(){
+			public String getText(){
+				int caretPosition = taField.getDocument().getLength();
+				Element root = taField.getDocument().getDefaultRootElement();
+				String text = " 1  " + System.getProperty("line.separator");
+				for(int i = 2; i < root.getElementIndex( caretPosition ) + 2; i++){
+					text += " " + i + "  " + System.getProperty("line.separator");
+				}
+				return text;
+			}
+			@Override
+			public void changedUpdate(DocumentEvent de) {
+				taLineNumber.setText(getText());
+			}
+ 
+			@Override
+			public void insertUpdate(DocumentEvent de) {
+				taLineNumber.setText(getText());
+			}
+ 
+			@Override
+			public void removeUpdate(DocumentEvent de) {
+				taLineNumber.setText(getText());
+			}
+ 
+		});
+		
+		scrollPane.setViewportView(taField);
+		scrollPane.setRowHeaderView(taLineNumber);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		btnRun = new JButton("\t\u25B6");
+		btnRun.setBounds(10, 11, 89, 23);
+		btnRun.addMouseListener(this);
+		contentPane.add(btnRun);
+		
+		spLexer = new JScrollPane();
+		spLexer.setBounds(698, 11, 254, 674);
+		contentPane.add(spLexer);
+		
+		taLexer = new JTextArea();
+		taLexer.setForeground(Color.RED);
+		taLexer.setFont(new Font("Consolas", Font.PLAIN, 13));
+		taLexer.setEditable(false);
+		taLexer.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		spLexer.setViewportView(taLexer);
+		
+		spResult = new JScrollPane();
+		spResult.setBounds(10, 470, 678, 215);
+		contentPane.add(spResult);
+		
+		taResult = new JTextArea();
+		taResult.setEditable(false);
+		spResult.setViewportView(taResult);
+
 	}
 
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		if(e.getSource().equals(btnRun)) {
+			MyVisitor mv = new MyVisitor();
+			System.out.println("Input is: ");
+			System.out.println(taField.getText().toString());
+			String output = mv.visit(taField.getText().toString());
+			taLexer.setText(output);
+		}
+	}
 }
--- src/packageA/MyVisitor.java
+++ src/packageA/MyVisitor.java
@@ -2,29 +2,32 @@ package packageA;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-
-import packageA.JavaParser.MethodDeclarationContext;
+import java.util.List;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import packageA.JavaParser.StatementContext;
 
 public class MyVisitor extends JavaBaseVisitor<Void> {
 
-    /**
-     * Main Method
-     * 
-     * @param args
-     * @throws IOException
-     */
-    public static void main(String[] args) throws IOException {
-        ANTLRInputStream input = new ANTLRInputStream(new FileInputStream("C:\\\\Users\\\\Winfred Villaluna\\\\Documents\\\\SE Workspace\\\\ANTLR JAVA Project\\\\src\\\\packageA\\\\MyVisitor.java")); // we'll
-                                                                                                                                                    // parse
-                                                                                                                                                    // this
-                                                                                                                                                    // file
+    public String visit(String userInput) {
+    	ANTLRInputStream input = new ANTLRInputStream(userInput);
+    	
+        StringBuilder sb = new StringBuilder("");
+        
         JavaLexer lexer = new JavaLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
+        for (Token token = lexer.nextToken(); token.getType() != Token.EOF; token = lexer.nextToken()) {
+            sb.append(token.getText())
+            	.append(" | ")
+            	.append(JavaLexer.VOCABULARY.getSymbolicName(token.getType()))
+            	.append(System.getProperty("line.separator"));
+        }
         JavaParser parser = new JavaParser(tokens);
         ParseTree tree = parser.compilationUnit(); // see the grammar ->
                                                     // starting point for
@@ -36,17 +39,32 @@ public class MyVisitor extends JavaBaseVisitor<Void> {
                                                 // and overrides the methods
                                                 // you're interested
         visitor.visit(tree);
+        
+        return sb.toString();
     }
 
     /**
      * some attribute comment
      */
     private String  someAttribute;
-
+    
     @Override
-    public Void visitMethodDeclaration(MethodDeclarationContext ctx) {
-        System.out.println("Method name:" + ctx.getText());
-        return super.visitMethodDeclaration(ctx);
+    public Void visitStatement(StatementContext ctx) {
+    	
+//    	int x=ctx.getChildCount();
+//    	for(int i = 0;i < x; i++) {
+//    		printChildren(ctx.getChild(i), ctx.getRuleContext().getChild(i));
+//    	}
+//    	
+    	return super.visitStatement(ctx);
     }
-
+    
+    public void printChildren(ParseTree pt, ParseTree rt) {
+    	for(int i = 0; i < pt.getChildCount(); i++) {
+    		printChildren(pt.getChild(i), rt.getChild(i));
+    		if(pt.getChild(i).getChildCount() == 0) {
+    			System.out.println(pt.getChild(i).getText() + " --> ");
+    		}
+    	}
+    }
 }
