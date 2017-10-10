29c46674faa644509b5bea29e002b06626ce6f39
Kyle-Althea <kyle-althea_santos@dlsu.edu.ph>
2017-10-11 02:24:31 +0800

    idk wtf happened

--- src/packageA/ANTLRDemo.java
+++ src/packageA/ANTLRDemo.java
@@ -3,7 +3,7 @@ import org.antlr.runtime.*;
 
 public class ANTLRDemo {
     public static void main(String[] args) throws Exception {
-    	String[] arg0 = { "-visitor", "C:\\Users\\rafe0\\OneDrive\\Documents\\COMPILER-PROJECT\\src\\packageA\\Java.g4", "-package", "packageA" };
+    	String[] arg0 = { "-visitor", "/Users/Thea/COMPILER-PROJECT/src/packageA/Java.g4", "-package", "packageA" };
         org.antlr.v4.Tool.main(arg0);
     }
 }
